c09d20539cdf4de1b31d6fff37c267c5ffa3b3c9
Виктор Ражный <rajniyviktor@yandex.ru>
2024-05-20 19:02:34 +0700

    feature(ApplicationContext): Добавлен класс контекста.

---
+++ src/PathfinderCharacterCreator.Server/DataBaseAccess/ApplicationContext.cs
@@ -0,0 +1,20 @@
+﻿namespace DataBaseAccess
+{
+    using Microsoft.EntityFrameworkCore;
+
+    public class ApplicationContext : DbContext
+    {
+        #region DbSet
+
+        #endregion
+
+        /// <summary>
+        /// Конструктор класса.
+        /// </summary>
+        public ApplicationContext(DbContextOptions<ApplicationContext> options)
+        : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
