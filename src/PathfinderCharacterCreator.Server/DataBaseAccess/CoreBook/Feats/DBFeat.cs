df2f7f144366b5f9546bb1eb5203004b8fd567c3
Виктор Ражный <rajniyviktor@yandex.ru>
2024-05-22 00:14:28 +0700

    feature(CoreBook/Feats): Добавлено представление черты в БД.

---
+++ src/PathfinderCharacterCreator.Server/DataBaseAccess/CoreBook/Feats/DBFeat.cs
@@ -0,0 +1,50 @@
+﻿namespace DataBaseAccess.CoreBook.Feats
+{
+    using DataBaseAccess.CoreBook.Traits;
+    using DataBaseAccess.CoreBook.Types;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.ComponentModel.DataAnnotations;
+
+    /// <summary>
+    /// Структура описывающая черту в базе данных.
+    /// </summary>
+    [Table("Feats")]
+    public class DBFeat
+    {
+        /// <summary>
+        /// Название черты на английском языке.
+        /// </summary>
+        [Key]
+        [Required]
+        public string Name { get; set; } = "???";
+
+        /// <summary>
+        /// Название черты на русском языке.
+        /// </summary>
+        [Required]
+        public string RussianName { get; set; } = "???";
+
+        /// <summary>
+        /// Описание черты.
+        /// </summary>
+        [Required]
+        public string Description { get; set; } = "???";
+
+        /// <summary>
+        /// Уровень черты.
+        /// </summary>
+        [Required]
+        public int Level { get; set; }
+
+        /// <summary>
+        /// Тип черты.
+        /// </summary>
+        [Required]
+        public FeatType Type { get; set; }
+
+        /// <summary>
+        /// Трейты черты.
+        /// </summary>
+        public List<FeatTraits>? Traits { get; set; }
+    }
+}
---
+++ src/PathfinderCharacterCreator.Server/DataBaseAccess/CoreBook/Feats/FeatBase.cs
@@ -0,0 +1,54 @@
+﻿namespace DataBaseAccess.CoreBook.Feats
+{
+    using DataBaseAccess.Character;
+    using DataBaseAccess.CoreBook.Types;
+    using System.Text.Json.Serialization;
+
+    /// <summary>
+    /// Черта и логика работы с ней.
+    /// </summary>
+    public class FeatBase
+    {
+        /// <summary>
+        /// Название черты персонажа.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Тип черты.
+        /// </summary>
+        public FeatType Type { get; set; }
+
+        /// <summary>
+        /// Требуемый уровень для получения черты.
+        /// </summary>
+        public int Level { get; set; }
+
+        /// <summary>
+        /// Делегат, возвращающий булевое значение, может ли персонаж 
+        /// иметь эту черту.
+        /// </summary>
+        /// <param name="info">Информация о персонаже.</param>
+        /// <returns>True - если персонаж может владить чертой.
+        /// False - если не может.</returns>
+        public delegate bool CanBeAssignDeleagte(DBCharacter info);
+
+        /// <summary>
+        /// Делегат, содержащий логику черты.
+        /// </summary>
+        /// <param name="characterInfo">Информация о персонаже для изменения.</param>
+        public delegate void AssignDelegate(DBCharacter characterInfo, CreationInfo creationInfo);
+
+        /// <summary>
+        /// Переменная делегата CanBeAssignDeleagte
+        /// </summary>
+        [JsonIgnore]
+        public CanBeAssignDeleagte CanAssign { get; set; }
+
+        /// <summary>
+        /// Переменная делегата AssignDelegate.
+        /// </summary>
+        [JsonIgnore]
+        public AssignDelegate Assign { get; set; }
+    }
+}
