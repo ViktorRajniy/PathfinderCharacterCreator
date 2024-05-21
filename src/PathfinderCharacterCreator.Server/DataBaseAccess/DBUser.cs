608d243f9b851ba2259bac6175b2b24d3bf85728
Виктор Ражный <rajniyviktor@yandex.ru>
2024-05-22 00:13:56 +0700

    feature(DBUser): Добавлено представление пользователя в БД.

---
+++ src/PathfinderCharacterCreator.Server/DataBaseAccess/DBUser.cs
@@ -0,0 +1,37 @@
+﻿namespace DataBaseAccess
+{
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.ComponentModel.DataAnnotations;
+    using DataBaseAccess.Character;
+
+    /// <summary>
+    /// Структура описывающая пользователя в базе данных.
+    /// </summary>
+    [Table("Users")]
+    public class DBUser
+    {
+        /// <summary>
+        /// ID пользователя.
+        /// </summary>
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Электронная почта пользователя.
+        /// </summary>
+        [Required]
+        public string Email { get; set; }
+
+        /// <summary>
+        /// Пароль пользователя
+        /// </summary>
+        [Required]
+        public string Password { get; set; }
+
+        /// <summary>
+        /// Список персонажей пользователя.
+        /// </summary>
+        public List<DBCharacter>? CharacterList { get; set; }
+    }
+}
