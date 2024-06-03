cb37a826619743f9f975bdf5286371d81e381833
Виктор Ражный <rajniyviktor@yandex.ru>
2024-06-03 13:01:56 +0700

    feature(CreationInfo): Изменена структура CreationInfo.

--- src/PathfinderCharacterCreator.Server/DataBaseAccess/ApplicationContext.cs
+++ src/PathfinderCharacterCreator.Server/DataBaseAccess/ApplicationContext.cs
@@ -41,7 +41,7 @@
         public ApplicationContext(DbContextOptions<ApplicationContext> options)
         : base(options)
         {
-            //Database.EnsureDeleted();
+            Database.EnsureDeleted();
             Database.EnsureCreated();
         }
 
