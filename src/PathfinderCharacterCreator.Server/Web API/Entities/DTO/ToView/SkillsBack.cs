c31f297139721c11c599cb270f7b6ee3859a7b5c
Виктор Ражный <rajniyviktor@yandex.ru>
2024-06-04 13:31:03 +0700

    feature(Cjntrollers): Добавлены Гет методы контроллерам.

---
+++ src/PathfinderCharacterCreator.Server/Web API/Entities/DTO/ToView/SkillsBack.cs
@@ -0,0 +1,11 @@
+﻿namespace Web_API.Entities.DTO
+{
+    using DataBaseAccess.CoreBook.Types;
+
+    public class SkillsBack
+    {
+        public int SkillCount { get; set; } = 0;
+
+        public List<SkillType> AllowedSkillsList { get; set; } = new List<SkillType>() { };
+    }
+}
