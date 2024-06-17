1b4567fff77169c6eb2783c2c805ba31ffb1beb7
Виктор Ражный <rajniyviktor@yandex.ru>
2024-06-17 18:25:24 +0700

    feature(ActionTraits): Добавлены поля в перечисление.

--- src/PathfinderCharacterCreator.Server/DataBaseAccess/CoreBook/Traits/ActionTraits.cs
+++ src/PathfinderCharacterCreator.Server/DataBaseAccess/CoreBook/Traits/ActionTraits.cs
@@ -14,5 +14,50 @@
         /// Движение
         /// </summary>
         Move,
+
+        /// <summary>
+        /// Натиск.
+        /// </summary>
+        Press,
+
+        /// <summary>
+        /// Начальный.
+        /// </summary>
+        Open,
+
+        /// <summary>
+        /// Стойка.
+        /// </summary>
+        Stance,
+
+        /// <summary>
+        /// Размах.
+        /// </summary>
+        Flourism,
+
+        /// <summary>
+        /// Ментальный.
+        /// </summary>
+        Mental,
+
+        /// <summary>
+        /// Метамагия.
+        /// </summary>
+        Metamagic,
+
+        /// <summary>
+        /// Концентрация.
+        /// </summary>
+        Concentrate,
+
+        /// <summary>
+        /// Аркана.
+        /// </summary>
+        Arcana,
+
+        /// <summary>
+        /// Преграждение.
+        /// </summary>
+        Abjuration,
     }
 }
